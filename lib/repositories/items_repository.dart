433621de77cf51706c3ce117fc1e4d6eb251c4fd
Ola Chmielewska <a.chmielewska01@gmail.com>
2022-06-07 18:42:20 +0200

    Add Details Page

--- lib/repositories/items_repository.dart
+++ lib/repositories/items_repository.dart
@@ -2,42 +2,49 @@ import 'package:cantwait28/models/item_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ItemsRepository {
-  Stream<List<ItemModel>>getItemsStream(){
+  Stream<List<ItemModel>> getItemsStream() {
     return FirebaseFirestore.instance
-    .collection('items')
-    .orderBy('relase_date')
-    .snapshots()
-    .map((querySnapshot){
+        .collection('items')
+        .orderBy('relase_date')
+        .snapshots()
+        .map((querySnapshot) {
       return querySnapshot.docs.map((doc) {
-          return ItemModel(
-            id: doc.id,
-            title: doc['title'],
-            imageURL: doc['image_url'],
-            relaseDate: (doc['relase_date'] as Timestamp).toDate(),
-          );
-        }).toList();
+        return ItemModel(
+          id: doc.id,
+          title: doc['title'],
+          imageURL: doc['image_url'],
+          relaseDate: (doc['relase_date'] as Timestamp).toDate(),
+        );
+      }).toList();
     });
   }
 
-Future<void>delete({required String id}){
-  return FirebaseFirestore.instance
-          .collection('items')
-          .doc(id)
-          .delete();
-}
+  Future<void> delete({required String id}) {
+    return FirebaseFirestore.instance.collection('items').doc(id).delete();
+  }
 
- Future<void> add(
+  Future<ItemModel> get({required String id}) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('items').doc(id).get();
+    return ItemModel(
+      id: doc.id,
+      title: doc['title'],
+      imageURL: doc['image_url'],
+      relaseDate: (doc['relase_date'] as Timestamp).toDate(),
+    );
+  }
+
+  Future<void> add(
     String title,
     String imageURL,
     DateTime releaseDate,
   ) async {
-      await FirebaseFirestore.instance.collection('items').add(
-        {
-          'title': title,
-          'image_url': imageURL,
-          'release_date': releaseDate,
-        },
-      );
-    
+    await FirebaseFirestore.instance.collection('items').add(
+      {
+        'title': title,
+        'image_url': imageURL,
+        'release_date': releaseDate,
+      },
+    );
   }
-  }
+}
